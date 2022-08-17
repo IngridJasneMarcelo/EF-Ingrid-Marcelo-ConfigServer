67a85bcf2c9def34a179dbeadc5bdf4531a12e5a
IngridJasneMarcelo <109398200+IngridJasneMarcelo@users.noreply.github.com>
2022-08-16 23:17:42 -0500

    Add files via upload

---
+++ src/test/java/com/marcelo/EFIngridMarceloConfigServer/EfIngridMarceloConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcelo.EFIngridMarceloConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfIngridMarceloConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
